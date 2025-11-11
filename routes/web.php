d1585d86f162606d74ac8be5b71f1167f14997e3
AstouMba <assalih1303@gmail.com>
2025-11-11 20:10:47 +0000

    swagger2

--- routes/web.php
+++ routes/web.php
@@ -5,11 +5,11 @@ use L5Swagger\Http\Controllers\SwaggerController;
 
 Route::get('/', fn () => view('welcome'));
 
-// Swagger UI
-Route::get('/api/documentation', [SwaggerController::class, 'api'])
-    ->name('l5-swagger.default.api');
+// // Swagger UI
+// Route::get('/api/documentation', [SwaggerController::class, 'api'])
+//     ->name('l5-swagger.default.api');
 
-// Fichier JSON Swagger
-Route::get('/docs', [SwaggerController::class, 'docs'])
-    ->name('l5-swagger.default.docs');
+// // Fichier JSON Swagger
+// Route::get('/docs', [SwaggerController::class, 'docs'])
+//     ->name('l5-swagger.default.docs');
 
